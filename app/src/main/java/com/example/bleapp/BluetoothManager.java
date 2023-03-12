1dc8871bb43500fc387401de532264604d4fedd4
hutao <13579taohu>
2023-03-12 14:19:58 +0800

    1、安卓Ble设备扫描

---
+++ app/src/main/java/com/example/bleapp/BluetoothManager.java
@@ -0,0 +1,13 @@
+package com.example.bleapp;
+
+public class BluetoothManager {
+    //服务
+    public static final String SERVER_UUID = "0000dfb0-0000-1000-8000-00805f9b34fb";
+
+    //特征
+    public static final String CHARACTERISTIC_01_UUID = "0000dfb1-0000-1000-8000-00805f9b34fb";//rwn
+    public static final String CHARACTERISTIC_02_UUID = "0000dfb2-0000-1000-8000-00805f9b34fb";//rwn
+    public static final String CHARACTERISTIC_03_UUID = "0000dfb3-0000-1000-8000-00805f9b34fb";//wn
+    public static final String CHARACTERISTIC_04_UUID = "0000dfb4-0000-1000-8000-00805f9b34fb";//n
+    public static final String CHARACTERISTIC_05_UUID = "0000dfb5-0000-1000-8000-00805f9b34fb";//r
+}
--- app/src/main/java/com/example/bleapp/MainActivity.java
+++ app/src/main/java/com/example/bleapp/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.bleapp;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     //获取系统蓝牙适配器管理类
     private BluetoothAdapter mBluetoothAdapter;
     private Intent mEnableBtIntent;
+    private BluetoothLeScanner mScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
             Log.i(TAG, "onCreate: not support bluetooth");
             finish();
             return;
+        } else {
+            mScanner = mBluetoothAdapter.getBluetoothLeScanner();
         }
         mEnableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 
@@ -50,19 +65,34 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             });
-            lun.launch(new String[]{Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_SCAN});
+            lun.launch(new String[]{
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            });
         } else {
             Log.i(TAG, "onCreate: else");
-            ActivityResultLauncher<String> lun = registerForActivityResult(new ActivityResultContracts.RequestPermission(), result -> {
-                if (result) {
-                    Log.i(TAG, "onActivityResult: 权限通过, 初始化扫描任务...");
-                    // 开启扫描
-                    startScanTask();
+            ActivityResultLauncher<String[]> lun = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+                AtomicBoolean isAllOk = new AtomicBoolean(true);
+                result.forEach((s, aBoolean) -> {
+                    if (!aBoolean) {
+                        isAllOk.set(false);
+                    }
+                });
+                if (isAllOk.get()) {
+                    Log.i(TAG, "onActivityResult: 权限通过, 初始化扫描");
+                    initScanTask();
                 } else {
                     Log.i(TAG, "onActivityResult: 权限未通过");
                 }
             });
-            lun.launch(Manifest.permission.BLUETOOTH_CONNECT);
+            lun.launch(new String[]{
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            });
         }
     }
 
@@ -74,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 1) {
             if (resultCode == RESULT_OK) {
                 Toast.makeText(this, "蓝牙已经开启", Toast.LENGTH_SHORT).show();
-                startScanTask();
+                initScanTask();
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "没有蓝牙权限", Toast.LENGTH_SHORT).show();
                 finish();
@@ -85,11 +115,29 @@ public class MainActivity extends AppCompatActivity {
     /**
      * 开启扫描任务
      */
-    private void startScanTask(){
+    @SuppressLint("MissingPermission")
+    private void initScanTask() {
         Log.i(TAG, "startScanTask: ");
+        List<ScanFilter> filterList = new ArrayList<>();
+        ScanFilter filter = new ScanFilter.Builder()
+//                .setDeviceName("Bluno")
+                .setDeviceAddress("88:33:14:DC:76:26")
+                .build();
+        filterList.add(filter);
+        ScanSettings settings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .build();
 
-
-
+        mScanner.startScan(filterList, settings, new ScanCallback() {
+            @Override
+            public void onScanResult(int callbackType, ScanResult result) {
+                super.onScanResult(callbackType, result);
+                Log.i(TAG, "onScanResult: result: " + result.getDevice().getName() + ", address:" + result.getDevice().getAddress());
+            }
+        });
     }
 
+    public void startScan(View view) {
+
+    }
 }
